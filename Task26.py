cf46f16c3f5ddc711895cfed3cf28ca3d9a9cc30
Prozhikin_Andrey <a.prozhikin@gmail.com>
2023-03-04 15:14:34 +0200

    Feat: Add for Task26 second variant with negative pow oppotunity

--- Task26.py
+++ Task26.py
@@ -6,6 +6,7 @@
 # A = 3; B = 5 -> 243 (3⁵)
 #     A = 2; B = 3 -> 8 
 
+# Вариант 1:
 def num_power(number, power):
     if power == 0:
         return 1
@@ -16,3 +17,20 @@ def num_power(number, power):
 a = int(input('Введите число А: '))
 b = int(input('Введите число B: '))
 print(num_power(a, b))
+
+# Вариант 2 (можно указывать отрицательную степень)
+
+
+def num_all_power(number, power):
+    if power == 0:
+        return 1
+    else:
+        if power >= 0:
+            return number*num_all_power(number, power-1)
+        else:
+            return 1/(number*num_all_power(number, -power-1))
+
+
+a = int(input('Введите число А: '))
+b = int(input('Введите число B: '))
+print(num_all_power(a, b))
